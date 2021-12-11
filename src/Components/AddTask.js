bbdcc442f66f1a083d1d33d48a1a434c294d67b5
Vedant Gune <vedantgune41@gmail.com>
2021-12-11 19:14:55 +0530

    Added a modal notifier

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,50 +1,75 @@
-import { useRef } from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Form, Button } from "react-bootstrap";
+import ModalAlert from "./UI/ModalAlert";
 
 const AddTask = (props) => {
-  const todoItemName = useRef("");
-  const todoItemDescription = useRef("");
+  const [todoItemName, setTodoItemName] = useState("");
+  const [todoItemDescription, setTodoItemDescription] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  const taskInputHandler = (event) => {
+    setTodoItemName(event.target.value);
+  };
+
+  const descInputHandler = (event) => {
+    setTodoItemDescription(event.target.value);
+  };
 
   const submitFormHandler = (event) => {
     event.preventDefault();
+    if (todoItemName.trim() === "") {
+      setShowModal(true);
+      return;
+    }
+
     props.onAddItem({
       id: Math.random() * 100,
-      title: todoItemName.current.value,
-      desc: todoItemDescription.current.value,
+      title: todoItemName,
+      desc: todoItemDescription,
     });
   };
 
-  return (
-    <Container className="my-5 py-5 px-5 w-75 border border-dark rounded">
-      <Form onSubmit={submitFormHandler}>
-        <Form.Group className="mb-3" controlId="ControlInput1">
-          <Form.Label>
-            <b>Todo Task : </b>
-          </Form.Label>
-          <Form.Control
-            type="text"
-            ref={todoItemName}
-            placeholder="title of todo item."
-          />
-        </Form.Group>
+  const hideModalHandler = (value) => {
+    setShowModal(value);
+  };
 
-        <Form.Group className="mb-3" controlId="ControlTextarea1">
-          <Form.Label>
-            <b>Description : </b>
-          </Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={3}
-            ref={todoItemDescription}
-            placeholder="A Brief Description..."
-          />
-        </Form.Group>
-        <Button type="submit" className="px-5 py-2 mt-2">
-          Add Task
-        </Button>
-      </Form>
-    </Container>
+  return (
+    <>
+      <Container className="my-5 py-5 px-5 w-75 border border-dark rounded">
+        <Form onSubmit={submitFormHandler}>
+          <Form.Group className="mb-3" controlId="ControlInput1">
+            <Form.Label>
+              <b>Todo Task : </b>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              className="input-bg red"
+              onChange={taskInputHandler}
+              value={todoItemName}
+              placeholder="title of todo item."
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="ControlTextarea1">
+            <Form.Label>
+              <b>Description : </b>
+            </Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              onChange={descInputHandler}
+              value={todoItemDescription}
+              placeholder="A Brief Description..."
+            />
+          </Form.Group>
+          {showModal && <ModalAlert onShowModal={hideModalHandler} />}
+          <Button type="submit" className="px-5 py-2 mt-2">
+            Add Task
+          </Button>
+        </Form>
+      </Container>
+    </>
   );
 };
 
---
+++ src/Components/UI/ModalAlert.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal, Button, ModalFooter, ModalBody } from "react-bootstrap";
+import ModalHeader from "react-bootstrap/esm/ModalHeader";
+import ModalTitle from "react-bootstrap/esm/ModalHeader";
+
+const ModalAlert = (props) => {
+  const hideModalHandler = () => {
+    props.onShowModal(false);
+  };
+
+  return (
+    <Modal show>
+      <ModalHeader>
+        <ModalTitle>Invalid TODO Item!</ModalTitle>
+      </ModalHeader>
+      <ModalBody>Please enter valid todo item!</ModalBody>
+      <ModalFooter>
+        <Button onClick={hideModalHandler}>Close Modal</Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default ModalAlert;
